881b7759c6c817ffbdf1f5609b3633b9eeef8094
Mikmik28 <mikeeagregado28@gmail.com>
2025-06-17 22:19:33 +0800

    Adds client service

---
+++ lib/client.rb
@@ -0,0 +1,30 @@
+class Client
+  attr_reader :full_name, :email
+
+  def initialize(attrs = {})
+    @full_name = attrs['full_name']
+    @first_name, @last_name = split_name
+    @email = attrs['email']
+  end
+
+  def split_name
+    parts = @full_name.to_s.strip.split
+    [parts.first, parts[1..].join(' ')]
+  end
+
+  def first_name
+    @first_name
+  end
+
+  def last_name
+    @last_name
+  end
+
+  def to_h
+    {
+      first_name: first_name,
+      last_name: last_name,
+      email: email
+    }
+  end
+end
